f9ae4ed2e0e3f1f35fd7c947499dfb22bd5ee7cf
Sundararajan Thangaraj <geeksundar1993@gmail.com>
2025-10-10 00:27:57 +0530

    Function intro

--- introduction_to_rust/src/main.rs
+++ introduction_to_rust/src/main.rs
@@ -10,4 +10,27 @@ fn main() {
     println!("{TAX_RATE}");
 
     data_types::data_types_fun();
+
+    // Invoking function
+
+    create_pizza();
+    delivering_pizza(); 
+    delivering_pizza(); // Reusing the Funtion
+    delivering_pizza(); // Reusing the Funtion
+    making_profit();
+    making_profit();  // Reusing the Funtion
+    making_profit(); // Reusing the Funtion
+}
+
+// Pizza business
+fn create_pizza() {
+    println!("Baking Pizzas");
+}
+
+fn delivering_pizza() {
+    println!("Delivered Pizza");
+}
+
+fn making_profit() {
+    println!("Making more profit");
 }
