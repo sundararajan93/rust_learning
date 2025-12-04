d21684de67779ce18e27b78f62aae348c2fcc508
Sundararajan Thangaraj <geeksundar1993@gmail.com>
2025-12-05 00:03:22 +0530

    Dependencies in rust and handle them in modules

--- dependencies_example/src/inventory/mod.rs
+++ dependencies_example/src/inventory/mod.rs
@@ -1,5 +1,8 @@
+// Using the external module fake and getting the Dummy Trait 
+// use Dummy Trait for the struct 
 use fake::Dummy;
 
+// Adding Dummy trait so that we shall create a dummy value for the struct
 #[derive(Debug, Dummy)]
 pub struct User {
     name: String,
